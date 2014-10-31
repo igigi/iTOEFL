db866f6205f01929257a635d35a55c6d3bf3bf64
rain <wuzhenyu@xiaoma.com>
2014-10-31 15:34:21 +0800

    per page 10

--- app/controllers/api/v1/vocabulary_groups.rb
+++ app/controllers/api/v1/vocabulary_groups.rb
@@ -11,7 +11,7 @@ module API
           requires :page, type: Integer, desc: "info of page"
         end
         get "", root: :vocabulary_questions do
-          VocabularyGroup.find_by(sequence_number: params[:sequence_number]).vocabulary_questions.paginate(page: params[:page])
+          VocabularyGroup.find_by(sequence_number: params[:sequence_number]).vocabulary_questions.paginate(page: params[:page], per_page: 10)
         end
       end
     end
