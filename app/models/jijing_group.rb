4e58e8d2a8908185e3327682377a5a4f549f2bb1
zongyannan <1203363236@qq.com>
2014-12-04 15:58:25 +0800

    jinghua & jijing question add score and type

--- app/models/jijing_group.rb
+++ app/models/jijing_group.rb
@@ -1,3 +1,44 @@
+# encoding: utf-8
+
 class JijingGroup < ActiveRecord::Base
 	has_many :jijing_questions
+
+  def belong_questions(question_type, user_id)
+    user = User.find(user_id)
+    belong_questions = jijing_questions.where(question_type: question_type)
+    questions = {jijing_questions: []}
+    belong_questions.each do |question|
+      case question_type
+      when '1'
+        answer = user.jijing_answers.where(jijing_question_id: question.id).last
+      when '2'
+        answer = user.articles.where(jijing_question_id: question.id).last
+      end
+
+      answer_id = nil
+      score = 0
+      # type = 0 未做, type =1 未批, type=2 已批
+      type = 0
+      if answer
+        status = answer.status
+        type = 1 if status == '0'
+        if status == '1'
+          type = 2 if status == '1'
+
+          case question_type
+          when '1'
+            score = answer.jijing_marks.last.score
+          when '2'
+            score = answer.article_marks.last.score
+          end
+
+        end
+        answer_id = answer.id if ['0', '3'].include?(status)
+      end
+      question_message = {id: question.id, sequence_number: question.sequence_number, content: question.content,
+        analysis: question.analysis, title: name, answer_id: answer_id, score: score, type: type}
+      questions[:jijing_questions].push(question_message)
+    end
+    questions
+  end
 end
--- app/models/jinghua_question.rb
+++ app/models/jinghua_question.rb
@@ -1,5 +1,33 @@
+# encoding: utf-8
+
 class JinghuaQuestion < ActiveRecord::Base
 	has_many :jinghua_answers, dependent: :destroy
 	has_many :jinghua_marks, through: :jinghua_answers
 	has_many :jinghua_samples, dependent: :destroy
+
+  def self.type_messages(content_type, user_id)
+    user = User.find(user_id)
+    type_questions = JinghuaQuestion.where(content_type: content_type)
+    # id, :content, :analysis, :answer_id#, :score, :type
+    questions = {jinghua_questions: []}
+    type_questions.each do |question|
+      answer = user.jinghua_answers.where(jinghua_question_id: question.id).last
+      answer_id = nil
+      score = 0
+      # type = 0 未做, type =1 未批, type=2 已批
+      type = 0
+      if answer
+        status = answer.status
+        type = 1 if status == '0'
+        if status == '1'
+          type = 2 if status == '1'
+          score = answer.jinghua_marks.last.score
+        end
+        answer_id = answer.id if ['0', '3'].include?(status)
+      end
+      question_message = {id: question.id, content: question.content, analysis: question.analysis, answer_id: answer_id, score: score, type: type}
+      questions[:jinghua_questions].push(question_message)
+    end
+    questions
+  end
 end
