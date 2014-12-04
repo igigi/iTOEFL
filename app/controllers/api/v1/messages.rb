69df1c84ed3a874f9135e52d1b4b511e953020f9
zongyannan <1203363236@qq.com>
2014-12-04 16:24:06 +0800

    modify message bug

--- app/controllers/api/v1/messages.rb
+++ app/controllers/api/v1/messages.rb
@@ -56,7 +56,7 @@ module API
         end
         post ":id/set_message_as_read" do
           authenticate!
-          message = Message.where(user_id: current_user.id, id: params[:id]).where.not(is_readed: [nil, true]).first
+          message = Message.where(user_id: current_user.id, id: params[:id]).where.not(is_readed: true).first
           if message
             message.update(is_readed: true)
             status 204
