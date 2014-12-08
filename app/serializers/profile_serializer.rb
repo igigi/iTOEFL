14ae066132594bea5738529f0cb326fb14e741b8
rain <wuzhenyu@xiaoma.com>
2014-12-08 15:11:11 +0800

    test profile avatar

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -2,6 +2,6 @@ class ProfileSerializer < ActiveModel::Serializer
   attributes :id, :avatar, :nickname, :gender, :grade, :phone, :role
 
   def avatar
-  	object.avatar_url
+  	object.avatar.url
   end
 end
