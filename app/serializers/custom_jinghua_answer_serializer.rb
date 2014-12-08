305bf4f155b067bc3612da9005f62994545022f3
rain <wuzhenyu@xiaoma.com>
2014-12-08 16:16:01 +0800

    test profile avatar

--- app/serializers/custom_jinghua_answer_serializer.rb
+++ app/serializers/custom_jinghua_answer_serializer.rb
@@ -1,8 +1,10 @@
 class CustomJinghuaAnswerSerializer < ActiveModel::Serializer
-  attributes :id, :content, :remark, :audio_length, :created_at, :user
+  attributes :id, :content, :remark, :audio_length, :created_at
+
+  has_one :user
 
   def user
-  	object.user.profile.avatar.url
+  	object.user.profile
   end
   has_one :jinghua_question
   has_many :jinghua_marks
--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,3 +1,7 @@
 class ProfileSerializer < ActiveModel::Serializer
   attributes :id, :avatar, :nickname, :gender, :grade, :phone, :role
+
+  def avatar
+  	object.avatar.url
+  end
 end
