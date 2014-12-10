70ad01d7759dc2fb94b1b5298debca13f4eceeb6
rain <wuzhenyu@xiaoma.com>
2014-12-10 09:15:10 +0800

    push_count bug

--- config/initializers/message_check.rb
+++ config/initializers/message_check.rb
@@ -3,8 +3,8 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.new
 
 scheduler.every '30s'  do
-    Message.all.each do |item|
-        PushWorker.perform_async(item.id) if !item.is_pushed && item.push_count < 3
+    Message.where("is_pushed = ? AND push_count < ?", false, 3).each do |item|
+        PushWorker.perform_async(item.id)
     end
 end
 
